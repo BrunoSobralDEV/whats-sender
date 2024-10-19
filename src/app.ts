7ab3a01bc9d859816dd9f93717e73c8117fa08b9
Bruno Sobral <brunosobralss@hotmail.com>
2024-10-19 10:18:36 -0300

    feat: typescript

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Express, urlencoded, json } from 'express';
 import cors from 'cors';
+import { routes } from './routes';
+import whatsappClient from './services/whatsappService';
 
 export const app: Express = express();
 
@@ -7,3 +9,5 @@ app.use(urlencoded({ extended: true }));
 app.use(json());
 app.use(json({ type: 'application/vnd.api+json' }));
 app.use(cors());
+
+app.use(routes);
